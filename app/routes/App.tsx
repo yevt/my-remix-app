d06ce2f9f75239f9b8d6e1756f8b9353e0972bbe
Auerael <auerael@gmail.com>
2024-02-01 06:38:59 +0300

    lets try complied tms format

--- app/routes/App.tsx
+++ app/routes/App.tsx
@@ -1,27 +1,11 @@
 import { useState } from "react";
 import { FormattedMessage, IntlProvider } from "react-intl"
-import Select from 'react-select'
+import ruLocaleMessages from '../lang/ru.json'
+import enLocaleMessages from '../lang/en.json'
 
 const localeIds = ['ru', 'en'] as const;
 
-const ruLocaleMessages = {
-  'localizedString1': 'Локализованная строка 1'
-}
-
-const enLocaleMessages = {
-  'localizedString1': 'Localized string 1'
-}
-
 const localeMessages = {ru: ruLocaleMessages, en: enLocaleMessages};
-
-const localeSelectOptions = [
-  {value: 'ru', label: 'Русский'},
-  {value: 'en', label: 'Английский'}
-] as const;
-
-
-type SelectedOption = { readonly value: "ru"; readonly label: "Русский"; } | { readonly value: "en"; readonly label: "Английский"; }
-
 
 export const App = () => {
   const [currentLocaleId, setCurrentLocaleId] = useState<'ru' | 'en'>(localeIds[0]);
