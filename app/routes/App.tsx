6afbd5838a9219d4a638fd31c31c401be0be3255
Auerael <auerael@gmail.com>
2024-02-02 12:42:23 +0300

    loc string 3

--- app/routes/App.tsx
+++ app/routes/App.tsx
@@ -17,6 +17,7 @@ export const App = () => {
       <IntlProvider key={currentLocaleId} locale={currentLocaleId} messages={currentLocaleMessages}>
         <FormattedMessage id='localizedString1' defaultMessage='default localized string' />
         <FormattedMessage id='localizedString2' defaultMessage='default localized string 2' />
+        <FormattedMessage id='localizedString3' defaultMessage='default localized string 3' />
       </IntlProvider>
     </div>
   )
